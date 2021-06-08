a3e7e52f3b27fb6a52f3b2755055b4541a41165f
Banzak-Uninter <leosantanabr@gmail.com>
2021-06-08 17:38:37 -0300

    criada função para receber as informações

--- api.js
+++ api.js
@@ -14,6 +14,9 @@ function run(event) {
         if (response.data.message != "Not Found") {
             localStorage.setItem("userName", response.data.login)
             localStorage.setItem("avatar", response.data.avatar_url)
+            localStorage.setItem("name", response.data.name)
+            localStorage.setItem("email", response.data.email)
+            localStorage.setItem("location", response.data.location)
             location.href = "user.html"
         }
         
@@ -23,9 +26,5 @@ function run(event) {
 
         console.log(error)
     })
-}
-
-function createContent() {
-    var userImg = document.createElement('img')
 }
 
--- userScripts.js
+++ userScripts.js
@@ -1,13 +1,15 @@
 var logoutButton = document.getElementById('exit')
 var userName = document.getElementById('userName')
 var avatar = document.getElementById('avatarImage')
-
+var githubName = document.getElementById('name')
+var githubEmail = document.getElementById('avatarImage')
+var githubLocation = document.getElementById('avatarImage')
 
 userName.textContent = localStorage.getItem("userName")
 
 logoutButton.addEventListener('click', function () {
     location.href = "index.html"
-    
+
 })
 
 avatar.src = localStorage.getItem("avatar")
