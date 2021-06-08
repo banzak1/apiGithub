675a158c6245e42804a1d80748b5ae673ff3be5c
Banzak-Uninter <leosantanabr@gmail.com>
2021-06-08 18:20:41 -0300

    função para apresentar as informações pessoais

--- userScripts.js
+++ userScripts.js
@@ -2,10 +2,13 @@ var logoutButton = document.getElementById('exit')
 var userName = document.getElementById('userName')
 var avatar = document.getElementById('avatarImage')
 var githubName = document.getElementById('name')
-var githubEmail = document.getElementById('avatarImage')
-var githubLocation = document.getElementById('avatarImage')
+var githubEmail = document.getElementById('email')
+var githubLocation = document.getElementById('location')
 
 userName.textContent = localStorage.getItem("userName")
+githubName.textContent = localStorage.getItem("name")
+githubEmail.textContent = localStorage.getItem("email")
+githubLocation.textContent = localStorage.getItem("location")
 
 logoutButton.addEventListener('click', function () {
     location.href = "index.html"
